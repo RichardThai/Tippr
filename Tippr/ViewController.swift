b43a1214af7312d0849fe71890214d67e4aef310
Richard Thai <Richard.Thai@icloud.com>
2019-10-01 23:52:19 -0700

    Initial Commit

---
+++ Tippr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippr
+//
+//  Created by Richard Thai on 10/1/19.
+//  Copyright © 2019 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
